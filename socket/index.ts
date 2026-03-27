b435a1850a99c9cd894bae732394407c29de670b
DavitAve <davitavetisyan222@gmail.com>
2026-03-27 16:55:49 +0300

    Try to fix socket issue

--- socket/index.ts
+++ socket/index.ts
@@ -3,10 +3,18 @@ import { registerHandlers } from "./handlers";
 
 export function initSocket(server: any) {
   const io = new Server(server, {
-    cors: { origin: "*" },
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+    transports: ["websocket", "polling"],
+    allowEIO3: true,
+    pingTimeout: 60000,
+    pingInterval: 25000,
   });
 
   io.on("connection", (socket) => {
+    console.log("New connection:", socket.id);
     registerHandlers(io, socket);
   });
 
